611b8d07d5cff27bb50f63157c535e31cd10d7f1
kishanhitk <kishansharma1231@gmail.com>
2021-05-09 18:00:06 +0530

    ✨ Add initiliaze app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { AuthProvider } from "../firebase/auth";
+import firebaseClient from "../firebase/firebaseClient";
 
 function MyApp({ Component, pageProps }: any) {
+  firebaseClient();
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
