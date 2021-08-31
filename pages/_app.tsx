2c97a8b0b3443f1633cc52d382144697cbe88212
kishanhitk <kishansharma1231@gmail.com>
2021-08-31 20:18:38 +0530

    feat: Add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,21 @@ import initAuth from "../utils/initAuth";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import theme from "../styles/theme";
+import dynamic from "next/dynamic";
+import "nprogress/nprogress.css";
 initAuth();
+const TopProgressBar = dynamic(
+  () => {
+    return import("./../components/TopProgressBar");
+  },
+  { ssr: false }
+);
 function MyApp({ Component, pageProps }: any) {
   firebaseClient();
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
+        <TopProgressBar />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
